28cf33bbaa7d77ec79ac6f116e60cd73d050ad43
Josh Marom <josh@elementor.com>
2021-03-09 19:07:54 +0200

    e-heading element

--- assets/dev/js/frontend/handlers/heading.js
+++ assets/dev/js/frontend/handlers/heading.js
@@ -1,8 +1,64 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, css, html } from 'lit-element';
 
-class ElementorHeading extends LitElement {
+export class ElementorHeading extends LitElement {
+	static get properties() {
+		return {
+			size: { type: String, reflect: true },
+		};
+	}
+
+	constructor() {
+		super();
+		this.size = '';
+	}
+
+	static get styles() {
+		return css`
+		:host {
+			color: var(--heading-color, inherit) !important;
+			--heading-font-size: inherit;
+		}
+
+		:is(.default, .small, .medium, .large, .xl, .xxl) ::slotted(*:is(h1, h2, h3, h4, h5, h6)) {
+			font-size: var(--heading-font-size) !important;
+		}
+
+		.elementor-widget-container ::slotted(h1, h2, h3, h4, h5, h6) {
+			color: var(--heading-color, inherit) !important;
+			padding: 0 !important;
+			margin: 0 !important;
+			line-height: 1;
+		}
+
+		:is(.elementor-widget-container) ::slotted(*:is(h1, h2, h3, h4, h5, h6)) a {
+			color: inherit !important;
+			font-size: inherit !important;
+			line-height: inherit  !important;
+		}
+
+		.small {
+			--heading-font-size: 15px;
+		}
+		.medium {
+			--heading-font-size: 19px;
+		}
+		.large {
+			--heading-font-size: 29px;
+		}
+		.xl {
+			--heading-font-size: 39px;
+		}
+		.xxl {
+			--heading-font-size: 59px;
+		}`;
+	}
+
 	render() {
-		return html`<slot name="heading"></slot>`;
+		return html`
+			<div class="elementor-widget-container ${ this.size }">
+				<slot/>
+			</div>
+		`;
 	}
 }
 customElements.define( 'e-heading', ElementorHeading );
